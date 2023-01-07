29bdc51192e00f8a9e03eaf941d33caf35df9f90
Damian Kurpiewski <blackbat13@gmail.com>
2023-01-07 10:28:38 +0100

    More refactorization

--- main.py
+++ main.py
@@ -8,30 +8,28 @@ HEIGHT = 600
 
 TITLE = "Pong Pygame Zero"
 
+VERTICAL_MARGIN = 40
+HORIZONTAL_MARGIN = 20
 BG_COLOR = (64, 64, 64)
 FG_COLOR = "yellow"
 
 """ VARIABLES """
 
 left = Actor("left")
-left.x = 20
+left.x = HORIZONTAL_MARGIN
 left.y = HEIGHT / 2
 left.vy = 5
 left.points = 0
 left.win = False
 
 right = Actor("right")
-right.x = WIDTH - 20
+right.x = WIDTH - HORIZONTAL_MARGIN
 right.y = HEIGHT / 2
 right.vy = 5
 right.points = 0
 right.win = False
 
 ball = Actor("ball")
-ball.x = WIDTH / 2
-ball.y = HEIGHT / 2
-ball.vx = 5
-ball.vy = 5
 
 
 """ DRAW """
@@ -39,7 +37,8 @@ ball.vy = 5
 
 def draw():
     screen.fill(BG_COLOR)
-    screen.draw.line((WIDTH / 2, 40), (WIDTH / 2, HEIGHT - 40), color=FG_COLOR)
+    screen.draw.line((WIDTH / 2, VERTICAL_MARGIN), (WIDTH / 2,
+                     HEIGHT - VERTICAL_MARGIN), color=FG_COLOR)
 
     left.draw()
     right.draw()
@@ -54,12 +53,13 @@ def draw_points():
     """
     screen.draw.text(f"Left: {left.points}",
                      color=FG_COLOR,
-                     center=(WIDTH / 4 - 20, 20),
+                     center=(WIDTH / 4 - HORIZONTAL_MARGIN, HORIZONTAL_MARGIN),
                      fontsize=48)
 
     screen.draw.text(f"Right: {right.points}",
                      color="yellow",
-                     center=(WIDTH / 2 + WIDTH / 4 - 20, 20),
+                     center=(WIDTH / 2 + WIDTH / 4 -
+                             HORIZONTAL_MARGIN, HORIZONTAL_MARGIN),
                      fontsize=48)
 
 
@@ -86,42 +86,53 @@ def update():
     if not (left.win or right.win):
         update_players()
         update_ball()
+        update_winner()
 
 
 def update_players():
     """Updates players positions based on the pressed keys
     """
-    if keyboard.w and left.top > 40:
+    if keyboard.w and left.top > VERTICAL_MARGIN:
         left.y -= left.vy
 
-    if keyboard.s and left.bottom < HEIGHT - 40:
+    if keyboard.s and left.bottom < HEIGHT - VERTICAL_MARGIN:
         left.y += left.vy
 
-    if keyboard.up and right.top > 40:
+    if keyboard.up and right.top > VERTICAL_MARGIN:
         right.y -= right.vy
 
-    if keyboard.down and right.bottom < HEIGHT - 40:
+    if keyboard.down and right.bottom < HEIGHT - VERTICAL_MARGIN:
         right.y += right.vy
 
 
 def update_ball():
-    """Updates ball position
+    """Updates ball
+    """
+    update_ball_position()
+    update_ball_collision()
+    update_ball_exit()
+
+
+def update_ball_position():
+    """Updates ball position on the screen
     """
     ball.x += ball.vx
     ball.y += ball.vy
 
-    if ball.top <= 40:
+
+def update_ball_collision():
+    """Checks for ball collisions and updates it movement
+    """
+    if ball.top <= VERTICAL_MARGIN or ball.bottom >= HEIGHT - VERTICAL_MARGIN:
         ball.vy *= -1
 
-    if ball.bottom >= HEIGHT - 40:
-        ball.vy *= -1
-
-    if left.colliderect(ball):
+    if left.colliderect(ball) or right.colliderect(ball):
         ball.vx *= -1
 
-    if right.colliderect(ball):
-        ball.vx *= -1
 
+def update_ball_exit():
+    """Checks if the ball exited on the left or right side and updates players points accordingly
+    """
     if ball.left <= 0:
         reset_ball()
         right.points += 1
@@ -130,6 +141,10 @@ def update_ball():
         reset_ball()
         left.points += 1
 
+
+def update_winner():
+    """Checks if one of the players won the game
+    """
     if right.points == 11:
         right.win = True
         ball.game_over = True
@@ -153,4 +168,5 @@ def reset_ball():
 
 """ INITIALIZATION """
 
+reset_ball()
 pgzrun.go()
